5d513fb431fdc08e4d8e56ccfb64fda1e133cbac
fei zhao <huangjianxia0101@gmail.com>
2025-09-27 22:27:47 +0800

    Update app.js

--- miniprogram/miniprogram/app.js
+++ miniprogram/miniprogram/app.js
@@ -7,7 +7,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       await wx.cloud.init({
-        env: 'h-care-5gggrsj59b495ec9', 
+        env: 'yourenvid', 
         traceUser: true
       })
     }
